f346950386d4f2771165d51a96f9b3fe2e2d24ce
Jon Gayle <110350118+PersonalBeast@users.noreply.github.com>
2023-03-27 05:55:32 -0700

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,92 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QGridLayout, QWidget, QMessageBox
+
+
+class TicTacToe(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        self.current_player = 'X'
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Tic Tac Toe")
+
+        # create a central widget and a grid layout
+        central_widget = QWidget()
+        grid_layout = QGridLayout()
+
+        # create buttons for each square on the board and add them to the grid layout
+        self.buttons = {}
+        for i in range(3):
+            for j in range(3):
+                button = QPushButton()
+                button.clicked.connect(lambda _, row=i, col=j: self.handle_button_click(row, col))
+                self.buttons[(i, j)] = button
+                grid_layout.addWidget(button, i, j)
+
+        # create a status label and add it to the grid layout
+        self.status_label = QLabel()
+        grid_layout.addWidget(self.status_label, 3, 0, 1, 3)
+
+        # set the central widget and the grid layout
+        central_widget.setLayout(grid_layout)
+        self.setCentralWidget(central_widget)
+
+        # show the main window
+        self.show()
+
+    def handle_button_click(self, row, col):
+        if self.board[row][col] == ' ':
+            self.board[row][col] = self.current_player
+            self.buttons[(row, col)].setText(self.current_player)
+            winner = self.check_winner()
+            if winner:
+                self.show_winner(winner)
+            elif self.check_tie():
+                self.show_tie()
+            else:
+                self.current_player = 'O' if self.current_player == 'X' else 'X'
+                self.status_label.setText(f"{self.current_player}'s turn")
+
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != ' ':
+                return self.board[i][0]
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != ' ':
+                return self.board[0][i]
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+            return self.board[0][0]
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+            return self.board[0][2]
+        return None
+
+    def check_tie(self):
+        for row in self.board:
+            for square in row:
+                if square == ' ':
+                    return False
+        return True
+
+    def show_winner(self, winner):
+        QMessageBox.about(self, "Game Over", f"{winner} wins!")
+        self.reset_board()
+
+    def show_tie(self):
+        QMessageBox.about(self, "Game Over", "It's a tie!")
+        self.reset_board()
+
+    def reset_board(self):
+        self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        for button in self.buttons.values():
+            button.setText('')
+        self.current_player = 'X'
+        self.status_label.setText(f"{self.current_player}'s turn")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    tictactoe =    TicTacToe()
+    sys.exit(app.exec_())
+
+
